ed3c68f662f49cfc7b8b8e78c533db394f2b326a
rchrd <26688060+rchrd-0@users.noreply.github.com>
2025-12-25 23:49:31 +0800

    (client): initial commit

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <main>ping</main>;
+}
+
+export default App;
---
+++ apps/client/vite.config.ts
@@ -0,0 +1,21 @@
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
