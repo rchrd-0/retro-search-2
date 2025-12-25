99392579f83c73bbdc13be4ba6077936fc3dac52
rchrd <26688060+rchrd-0@users.noreply.github.com>
2025-12-25 18:50:17 +0800

    feat(api): base error handling

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,44 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
+app.use("*", logger());
+
+app.use("*", cors());
+
+app.notFound((c) => {
+  return c.json(
+    {
+      success: false,
+      message: "Not found",
+    },
+    404,
+  );
+});
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return c.json(
+      {
+        success: false,
+        message: err.message,
+      },
+      err.status,
+    );
+  }
+
+  console.error("Unhandled error");
+  return c.json(
+    {
+      success: false,
+      message: "Internal server error",
+    },
+    500,
+  );
+});
 
 app.get("/", (c) => {
   return c.json({
