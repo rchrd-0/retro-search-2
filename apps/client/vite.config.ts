b315ebfa098e8c5a117fbd20ab4a0ac8cb93247f
rchrd <26688060+rchrd-0@users.noreply.github.com>
2025-12-27 15:18:26 +0800

    chore: build scripts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,11 +1,9 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
+export default defineConfig(() => {
   return {
     plugins: [
       react({
